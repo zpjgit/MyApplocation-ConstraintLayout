e3559a3c8b36a1e20801856877243ce2fa7bf86a
zpjgit <zpj.love@foxmail.com>
2018-01-18 20:31:09 +0800

    ConstraintLayout test

---
+++ MyApplication/app/src/main/java/com/zpj/mytestapp1/FirstActivity.java
@@ -0,0 +1,12 @@
+package com.zpj.mytestapp1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
